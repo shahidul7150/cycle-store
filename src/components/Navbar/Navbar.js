583abb67be3d2ef29abb32b6f0678283e93eb245
shahidul7150 <shahidulislam7150@gmail.com>
2022-10-23 20:30:39 +0600

    create mobile menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FcShop } from "react-icons/fc";
+import { IoMdMenu } from "react-icons/io";
 
 import { connect } from "react-redux";
 
@@ -32,16 +33,16 @@ const Navbar = ({ cart }) => {
     // </div>
 
     <div className="md:px-12">
-      <a className="btn btn-ghost normal-case text-xl pl-0">daisyUI</a>
+      <a className="btn btn-ghost normal-case text-2xl font-extrabold pl-0">Rider Store</a>
       <div className=" flex lg:flex-row-reverse text-white  ">
 
         <div className="navbar-start">
 
           <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            <label tabIndex={0} className="btn btn-ghost lg:hidden pl-0">
+              <IoMdMenu className="text-2xl" />
             </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-[#579df84f] rounded-box w-52">
               <li>
                 <Link to="/cart">
                   <div className="flex jusi" >
